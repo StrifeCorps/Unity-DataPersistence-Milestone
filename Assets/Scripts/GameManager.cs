1cf3f6b790621c7cf21e2c73612b0f4ac6f587f2
StrifeCorps <strifecorps@gmail.com>
2023-09-18 21:06:36 -0500

    Implemented Main Menu scene and additional UI elements.
    UI still needs scripting for persistent data.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	class SaveData
+	{
+		
+	}
+
+	public void Save()
+	{
+
+	}
+
+	public void Load()
+	{
+
+	}
+}
